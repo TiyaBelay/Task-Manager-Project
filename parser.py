0204124a96cbd61fe8c725e9453c1610d626a3c5
Tiya <yaleb.t@gmail.com>
2017-01-30 16:34:39 -0800

    Remove try and except error handler

--- parser.py
+++ parser.py
@@ -6,46 +6,45 @@ from model import User, Email, Task, connect_to_db, db
 def get_payload_headers(gmail_service, query):
         """List Messages of the user's inbox matching the query."""
 
-        try:
-            results = gmail_service.users().messages().list(userId='me', q=query).execute()
-            msgs = results.get('messages', [])
-            headers_dict = {}
+        results = gmail_service.users().messages().list(userId='me', q=query).execute()
+        msgs = results.get('messages', [])
+        headers_dict = {}
 
-            for msg in msgs:
-                message_id_headers = {} #this dict is used to add the msg id as the key to the message dict payload_headers
-                message_info = get_message_by_id(gmail_service, 'me', msg['id']) #msg ids and threadids
-                message_id = str(message_info['id'])
+        for msg in msgs:
+            message_id_headers = {} #this dict is used to add the msg id as the key to the message dict payload_headers
+            message_info = get_message_by_id(gmail_service, 'me', msg['id']) #msg ids and threadids
+            message_id = str(message_info['id'])
 
 
-                header_dict = {}
-                payload_headers = message_info['payload']['headers']
-                message_id_headers[message_id] = payload_headers
+            header_dict = {}
+            payload_headers = message_info['payload']['headers']
+            message_id_headers[message_id] = payload_headers
 
-                for key, value in message_id_headers.items():
-                    for dict_items in value:
-                        key = dict_items['name']
-                        value = dict_items['value']
-                        if key in ['Subject', 'From', 'Date']: #This is only grabbing 1 message instead of all messages
-                            header_dict[key] = value
+            for key, value in message_id_headers.items():
+                for dict_items in value:
+                    key = dict_items['name']
+                    value = dict_items['value']
+                    if key in ['Subject', 'From', 'Date']: #This is only grabbing 1 message instead of all messages
+                        header_dict[key] = value
 
-                    for key in message_id_headers:
-                        headers_dict[key] = header_dict
+                for key in message_id_headers:
+                    headers_dict[key] = header_dict
 
-                    date_str = header_dict['Date']
-                    From = header_dict['From']
-                    Subject = header_dict['Subject']
-                    msg_id = key
+                date_str = header_dict['Date']
+                From = header_dict['From']
+                Subject = header_dict['Subject']
+                msg_id = key
 
-                    emailpresentindb = db.session.query(Email).filter(Email.email_id == msg_id).first()
+                emailpresentindb = db.session.query(Email).filter(Email.email_id == msg_id).first()
 
-                    if emailpresentindb is None:
-                        message = Email(email_id=msg_id, subject=Subject, sender_email=From, received_at=date_str)
-                        db.session.add(message)
-                        db.session.commit()
+                if emailpresentindb is None:
+                    message = Email(email_id=msg_id, subject=Subject, sender_email=From, received_at=date_str)
+                    db.session.add(message)
+                    db.session.commit()
 
-                    header_dict['Email'] = db.session.query(Email).filter(Email.email_id == msg_id).first()
+                header_dict['Email'] = db.session.query(Email).filter(Email.email_id == msg_id).first()
 
-            return headers_dict
+        return headers_dict
 
 def get_message_by_id(service, user_id, msg_id):
     message = service.users().messages().get(userId=user_id, id=msg_id, format='full').execute()
@@ -71,4 +70,3 @@ def msg_body(gmail_service, msg_id):
             filename = part.get_filename()
             counter = 1
         return message
-
